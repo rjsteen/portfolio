cf8ab63831ea8117bebb038fd3f8bbd4e19fb923
Richard Steen <greatresolutions@gmail.com>
2021-05-29 08:16:08 -0500

    first part of experience

--- components/Sections/Experience.vue
+++ components/Sections/Experience.vue
@@ -4,9 +4,9 @@
 
     <div class="resume-item d-flex flex-column flex-md-row mb-5">
       <div class="resume-content mr-auto">
-        <h3 class="mb-0">Pos</h3>
-        <div class="subheading mb-3">companies</div>
-        <p>Did many things</p>
+        <h3 class="mb-0">CoverMyMeds</h3>
+        <div class="subheading mb-3">Senior Software Engineer</div>
+        <p>Ruby and Elixir</p>
       </div>
       <div class="resume-date text-md-right">
         <span class="text-primary">Many Days</span>
